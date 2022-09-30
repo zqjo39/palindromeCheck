3871265688420bbe9371a9e99fc9fe146aece0ee
zqjo <zqjo@fresnou.org>
2022-09-30 11:35:00 -0700

    challenges!!!! (they're kinda hard and also what do you mean we can use the internet to look up solutions to our problems-)

--- routes/index.js
+++ routes/index.js
@@ -12,20 +12,17 @@ router.get('/', function(req, res) {
 router.post('/', function(req, res) {
   res.render('index', {
     phrase: req.body.userText,
+    reverse: [],
     message: getResultDescription(req.body.userText)
   })
 })
 
 function checkPalindrome(phrase) {
+  phrase = phrase.replace(/ |\?|\.|,|:|;|!|'|"/gi, "")
   let temp = phrase.split("");
   temp = temp.reverse();
-  temp = temp.join();
-  console.log(`phrase: ${phrase}`)
-  myPhrase = phrase.toLowerCase().replace(" ", "");
-  console.log(`fixed phrase: ${myPhrase}`);
-  console.log(`temp: ${temp}`)
-  myTemp = temp.toLowerCase().replace(" ", "");
-  console.log(`fixed temp: ${myTemp}`);
+  temp = temp.join("");
+  console.log(temp)
   if(phrase.toLowerCase().replace(" ", "") === temp.toLowerCase().replace(" ", "")) {
     return true
   } else {
